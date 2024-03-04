731d37dff67514f47799f6a14ce7a964353fa8c7
Azmain Morshed <114017420+azmainm@users.noreply.github.com>
2024-03-04 13:54:15 +0600

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,38 +18,88 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('LSA', style: TextStyle(color: CupertinoColors.systemRed)),
-        backgroundColor: CupertinoColors.systemGrey.withOpacity(0.5),
-        leading: Container(), // Placeholder to center the title
-      ),
+      backgroundColor: const Color.fromARGB(255, 129, 21, 148), // Set the background color to purple
       child: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset('assets/logos/Eve_Logo.png'), // Replace with your actual logo path
-            SizedBox(height: 50), // Provide some spacing
-            Text(
-              'Your academic success starts here.',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Image.asset('assets/logos/USAL_Logo.png', scale: 1.5), // Logo made smaller
             ),
-            SizedBox(height: 100), // Provide spacing
-CupertinoButton(
-  child: Text('Log in'),
-  color: CupertinoColors.systemRed,
-  onPressed: () {
-    // TODO: Implement login action
-  },
-),
-SizedBox(height: 10), // Provide some spacing
-CupertinoButton(
-  child: Text('Sign up'),
-  color: CupertinoColors.systemRed,
-  onPressed: () {
-    // TODO: Implement sign-up action
-  },
-),
-
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8), // Adjust the border radius as needed
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 10,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min, // Use min to take up only required space
+                children: [
+                  SizedBox(height: 20),
+                  Image.asset('assets/logos/Eve_Logo.png', scale: 1), 
+                  SizedBox(height: 10), // Provide some spacing
+                  RichText(
+                    text: TextSpan(
+                      text: 'Your academic success starts ',
+                      style: TextStyle(
+                        fontSize: 38,
+                        fontWeight: FontWeight.w900, // Extra bold
+                        color: Color.fromARGB(111, 0, 70, 128), // Blue color
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'here.',
+                          style: TextStyle(
+                            fontSize:38,
+                            fontWeight: FontWeight.w900, // Extra bold
+                            color: Color.fromARGB(255, 176, 7, 41), // Red color
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 30), // Provide spacing
+                  CupertinoButton(
+                    child: SizedBox(
+                      width: double.infinity, // Button width increased
+                      child: Text(
+                        'Log in',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    color: Color.fromARGB(255, 176, 7, 41),
+                    onPressed: () {
+                      // TODO: Implement login action
+                    },
+                  ),
+                  SizedBox(height: 10), // Provide some spacing
+                  CupertinoButton(
+                    child: SizedBox(
+                      width: double.infinity, // Button width increased
+                      child: Text(
+                        'Sign up',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    color: Color.fromARGB(255, 176, 7, 41),
+                    onPressed: () {
+                      // TODO: Implement sign-up action
+                    },
+                  ),
+                  Expanded(child: SizedBox()), // Pushes the content up
+                ],
+              ),
+            ),
           ],
         ),
       ),
